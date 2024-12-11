ae93c313b82cbfbffeb69706cd423ed5f224de76
berryget <berry@berry.place>
2024-12-11 03:57:19 +0000

    ratelimit stuff

--- auth.py
+++ auth.py
@@ -15,7 +15,11 @@ cred = credentials.Certificate({
     "type": "service_account",
     "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
     "private_key": os.environ.get("FIREBASE_PRIVATE_KEY", "").replace("\\n", "\n"),
-    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL")
+    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": f"https://www.googleapis.com/robot/v1/metadata/x509/{os.environ.get('FIREBASE_CLIENT_EMAIL', '').replace('@', '%40')}"
 })
 firebase_admin.initialize_app(cred)
 db = firestore.client()
